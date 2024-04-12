b47cee24db11d15ecbae52d68e29e634e0590dc1
Hoang-An1109 <phamhoangan11092001@gmail.com>
2024-04-12 16:17:44 +0700

    Thêm mệnh đề  Where

--- QuanLyQuanCafe.sql
+++ QuanLyQuanCafe.sql
@@ -25,7 +25,7 @@ truncate table categoryProduct;
 
 select * from categoryProduct;
 
-insert into product (nameProduct,idCategory,quantity,price) value ('Cafe Đen',1,10,25000);
+insert into product (nameProduct,idCategory,quantity,price) value ('Sữa chua cafe',1,10,25000);
 insert into product (nameProduct,idCategory,quantity,price) value ('Cafe Sữa',1,10,25000);
 insert into product (nameProduct,idCategory,quantity,price) value ('Bạc Xỉu',1,10,29000);
 insert into product (nameProduct,idCategory,quantity,price) value ('Capucino/Latte',2,10,35000);
@@ -45,6 +45,27 @@ select idProduct,nameProduct,quantity,price from product;
 select * from product;
 select idProduct,nameProduct,nameCategory,quantity,price from product inner join categoryProduct on product.idCategory=categoryProduct.idCategory;
 select idproduct,nameproduct,namecategory,quantity,price from product inner join categoryproduct on product.idcategory=categoryproduct.idcategory;
+
+SELECT * FROM product WHERE price=25000;
+
+SELECT * FROM product WHERE price>25000;
+
+SELECT * FROM product WHERE price=35000 AND idCategory=4;
+
+SELECT * FROM product WHERE idCategory=1 OR idCategory=2;
+
+SELECT * FROM product WHERE idCategory IN (1,2);
+
+SELECT * FROM product WHERE price BETWEEN 25000 AND 35000;
+
+SELECT * FROM product WHERE nameProduct LIKE '%cafe%';
+select * from product;
+SELECT * FROM product WHERE idCategory NOT IN (1,2);
+
+SELECT * FROM product WHERE price NOT BETWEEN 25000 AND 35000;
+
+SELECT * FROM product WHERE nameProduct NOT LIKE '%cafe%';
+
 
 drop database quanlysanpham;
 
